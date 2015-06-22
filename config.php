963a4b4a3fdfe201579802d28ab83c34eaeb873a
Ade Yusuf Yaumul Isnain <yaumulisnain@icloud.com>
2015-06-22 14:09:24 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+define('BASEURL', ''); #Your sites
+
+#DEFINE PATH
+define("CLASS_PATH", "lib");
+define('APP_FOLDER', 'app');
+define('DB_FOLDER', 'db');
+define('BIN_FOLDER', 'bin');
+
+#DEFINE DB
+define('DB_HOST', 'localhost');
+define("DB_USER", "root");
+define('DB_PASS', '');
+define('DB_NAME', '');
